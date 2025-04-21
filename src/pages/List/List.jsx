7c6892469b0248b819ab7268f16f23381d550df0
Mithlesh341 <mithleshmourya2004@gmail.com>
2025-04-22 00:28:28 +0530

    new changes

--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -173,7 +173,7 @@ const [status,setStatus]=useState("Pending");
       
     // </div> 
     <div className='list add flex-col'>
-    <h1 style={{textAlign:'center',marginTop:2,marginBottom:2,paddingBottom:5}}>All Foods List</h1>
+    <h1 style={{textAlign:'center',marginTop:2,marginBottom:2,paddingBottom:5}}>All Reviews List</h1>
     {/* <p>All Reviews List</p> */}
     <div className="list-table">
       <div className="list-table-format title">
